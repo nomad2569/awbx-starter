c9f4e0a60d52a2394554204bc9636edd33d89918
Kim <alsrb001218@naver.com>
2020-07-15 17:40:43 +0900

    edit icon

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -11,7 +11,7 @@ module.exports = {
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/frogpunch.jpg`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
